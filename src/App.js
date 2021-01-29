4ddbf7c729761ffb51d294a77ef4a5619ab3c17e
TwanMulder <twan.mulder@gmail.com>
2021-01-29 20:26:44 +0100

    add first test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import Links from "./links";
+
+const Item = (props) => {
+  const { item } = props;
+
+  return (
+    <article>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      <a href={item.link} target="_blank" rel="noreferrer noopener">
+        View Item
+      </a>
+    </article>
+  );
+};
+
+const Category = (props) => {
+  const { category } = props;
+
+  return (
+    <section>
+      <h2 className="text-white text-center text-2xl font-bold">{category.name}</h2>
+      {category.items.map((item, i) => {
+        return <Item item={item} key={i} />;
+      })}
+    </section>
+  );
+};
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="container mx-auto px-4 py-32">
+        <h1 className="text-center text-white text-3xl font-bold mb-2">Twan.Party 🥳</h1>
+        <h2 className="text-center text-gray-200 text-lg">
+          No more boring online hangout sessions, find something <span className="text-purple-500 uppercase font-bold">fun</span> to do!
+        </h2>
+      </header>
+      <main className="container mx-auto px-4">
+        {Links.map((category, i) => {
+          return <Category category={category} key={i} />;
+        })}
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/links.js
@@ -0,0 +1,16 @@
+const Links = [
+  {
+    name: "Word Games",
+    items: [
+      {
+        title: "Boggle eWord",
+        description: "eWord Challenge is an online reboot of Boggle word game available in English, Spanish, Filipino and French.",
+        link: "https://www.ewordchallenge.net/",
+        players: "2+",
+        paid: false,
+      },
+    ],
+  },
+];
+
+export default Links;

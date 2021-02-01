5573d09ceb840cca5996b378823b468a28f68a06
TwanMulder <twan.mulder@gmail.com>
2021-02-02 00:06:28 +0100

    create new filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import { Users, DollarSign } from "react-feather";
+import { useState, useEffect } from "react";
+import { Users, DollarSign, Minus, Plus } from "react-feather";
 import Sparkles from "./sparkles";
 
 import Links from "./links";
-const freeItems = Links.map((cat) => {
-  return { ...cat, items: cat.items.filter((items) => items.paid === false) };
-});
 
 const Item = (props) => {
   let { item } = props;
@@ -61,6 +58,87 @@ const Categories = (props) => {
 
 function App() {
   const [showFreeItems, setShowFreeItems] = useState(false);
+  const [amountOfPlayers, setAmountOfPlayers] = useState(0);
+  const [linksArray, setLinksArray] = useState(Links);
+
+  useEffect(() => {
+    let linksArray = Links;
+
+    const filters = {
+      free: showFreeItems,
+      players: Number(amountOfPlayers),
+    };
+
+    if (filters.free) {
+      linksArray = linksArray.map((Link) => {
+        return { ...Link, items: Link.items.filter((item) => !item.paid) };
+      });
+    }
+
+    if (filters.players !== 0) {
+      linksArray = linksArray.map((Link) => {
+        return {
+          ...Link,
+          items: Link.items.filter((item) => {
+            var player_amount = item.players.split(/[.*+-/_]/);
+            var minimum = player_amount[0];
+            if (filters.players >= minimum) {
+              var maximum = player_amount[1];
+              if (maximum) {
+                return filters.players <= maximum;
+              }
+              return true;
+            }
+            return false;
+          }),
+        };
+      });
+    }
+
+    linksArray = linksArray.filter((Link) => {
+      return Link.items.length;
+    });
+
+    return setLinksArray(linksArray);
+  }, [showFreeItems, amountOfPlayers]);
+
+  const handleAmountOfPlayersChange = (event, operation) => {
+    if (operation === "-") {
+      return setAmountOfPlayers(Number(amountOfPlayers) > 0 ? Number(amountOfPlayers) - 1 : 0);
+    }
+    if (operation === "+") {
+      return setAmountOfPlayers(Number(amountOfPlayers) < 100 ? Number(amountOfPlayers) + 1 : 100);
+    }
+
+    let { value } = event.target;
+
+    // Return if the value includes a non-numeric keystroke
+    const regex = /[0-9]|\./;
+    if (!regex.test(value) && value !== "") {
+      return;
+    }
+
+    // Return if number is above 100
+    if (value > 100) {
+      return;
+    }
+
+    // Return if number is below 0
+    if (value < 0) {
+      return;
+    }
+
+    // Remove stupid zeros
+    if (value[0] === "0" && value.length > 1) {
+      value = value.substring(1);
+    }
+
+    if (value === "0") {
+      value = 0;
+    }
+
+    setAmountOfPlayers(value);
+  };
 
   return (
     <>
@@ -77,22 +155,53 @@ function App() {
         </h2>
       </header>
       <main className="container mx-auto px-4">
-        <div className="flex items-center justify-between md:justify-center mb-6">
-          <p className="text-white md:mr-4">Show free items only</p>
-          <label className="switch">
-            <input
-              type="checkbox"
-              name="free"
-              checked={showFreeItems}
-              onChange={() => {
-                setShowFreeItems(!showFreeItems);
-              }}
-            />
-            <span className="slider"></span>
-          </label>
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+          <div className="flex items-center justify-between md:justify-center mb-8">
+            <p className="text-white md:mr-4">Show free items only</p>
+            <label className="switch">
+              <input
+                type="checkbox"
+                name="free"
+                checked={showFreeItems}
+                onChange={() => {
+                  setShowFreeItems(!showFreeItems);
+                }}
+              />
+              <span className="slider"></span>
+            </label>
+          </div>
+          <div className="text-white flex items-center justify-between md:justify-center mb-8">
+            <p className="md:mr-4">
+              How many people <span className="hidden md:inline">are playing</span>?
+            </p>
+            <div className="flex items-center select-none">
+              <Minus
+                className="cursor-pointer hover:opacity-50 transition-opacity"
+                onClick={(e) => {
+                  handleAmountOfPlayersChange(e, "-");
+                }}
+              />
+              <label>
+                <input
+                  className="w-10 h-10 mx-4 text-xl rounded-md bg-gray-800 text-center appearance-none m-0 focus:outline-none focus:ring focus:ring-purple-500"
+                  style={amountOfPlayers === 0 ? { color: "#4b5563" } : null}
+                  type="number"
+                  name="people"
+                  value={amountOfPlayers}
+                  onChange={handleAmountOfPlayersChange}
+                />
+              </label>
+              <Plus
+                className="cursor-pointer hover:opacity-50 transition-opacity"
+                onClick={(e) => {
+                  handleAmountOfPlayersChange(e, "+");
+                }}
+              />
+            </div>
+          </div>
         </div>
 
-        <Categories categories={showFreeItems ? freeItems : Links} />
+        <Categories categories={linksArray} />
       </main>
     </>
   );
